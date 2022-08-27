1775a915b88984c2af3a4067bb46ae8b35dbda20
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-27 13:38:30 +0600

    database connection

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ import dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 const port = 8000;
-const connect = () =>{
-  
+const connect = async () =>{
+  try{
+    await moongoose.config(process.env.MONGO)
+  } catch(error){
+    throw error;
+  }
+
 }
 
 app.listen(port, () => {
