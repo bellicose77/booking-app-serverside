9d0b981000672ce02b3497de3fc958b900f3fe6b
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-27 13:35:48 +0600

    i ma bak

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
 const app = express();
 dotenv.config();
 const port = 8000;
+const connect = () =>{
+  
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
