1eaa331c6a3ced72235ff7f029699e672601ef56
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-29 11:13:35 +0600

    hotel post api

--- routes/hotels.js
+++ routes/hotels.js
@@ -7,10 +7,11 @@ const router = express.Router()
 router.post("/",async(req,res)=>{
     const newHotel = new Hotel(req.body)
     try{
-        const savedHotel = await newHotel.save()
+        const savedHotel = await newHotel.save();
+        res.status(200).json(savedHotel);
 
     }catch(err){
-
+          res.status(500).json(err);
     }
 })
 
