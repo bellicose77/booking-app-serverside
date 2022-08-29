c05760997516ae7bd436ba32f38201a21f5f698e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-08-29 11:11:18 +0600

    hotel post api

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,13 +1,15 @@
 import express from 'express';
+import Hotel from '../models/Hotel';
 
 const router = express.Router()
 
 // Create api
 router.post("/",async(req,res)=>{
+    const newHotel = new Hotel(req.body)
     try{
 
     }catch(err){
-        
+
     }
 })
 
