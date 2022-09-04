2d5022648ccf945c64fd0d74fcd38c766483190e
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-04 12:16:33 +0600

    controllers done

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,6 +1,7 @@
 import express from 'express';
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from '../controllers/hotel.js';
-import Hotel from '../models/Hotel.js';
+import 
+{ createHotel, deleteHotel, getHotel, getHotels, updateHotel } 
+from '../controllers/hotel.js';
 import { createError } from '../utils/error.js';
 
 const router = express.Router()
