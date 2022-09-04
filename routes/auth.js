7157d4927e6fdadd7a5b801cc199549ff6b4ea93
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-04 12:47:04 +0600

    register route

--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,5 @@ import express from 'express';
 
 const router = express.Router()
 
-router.get('/',(req,res)=>{
-    res.send("auth api is here")
-});
-router.get('/register',(req,res)=>{
-    res.send("auth register is here")
-})
+router.get('/register')
 export default router
