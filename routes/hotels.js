2d751b5c7a2e6301bd2e477edc76eb929f59f1ba
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-04 11:50:18 +0600

    error middel ware done

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import Hotel from '../models/Hotel.js';
+import { createError } from '../utils/error.js';
 
 const router = express.Router()
 
@@ -54,20 +55,14 @@ router.get('/:id',async(req,res)=>{
 
 // get all data
 router.get('/',async(req,res,next)=>{
-    const failed = true;
-    const err = new Error()
-    err.status = 404;
-    err.message = "Sorry not found";
-    if(failed) return next(err)
    
     try{
-        //const hotels = await Hotel.find()
-        const hotels = await Hotel.findById('asdddfs')
+       const hotels = await Hotel.find()
         res.status(200).json(hotels);
 
     }catch(err){
-          //res.status(500).json(err);
-          next(err);
+          res.status(500).json(err);
+          //next(err);
     }
 })
 
--- utils/error.js
+++ utils/error.js
@@ -1,3 +1,6 @@
-export const createError = ()=>{
-    
+export const createError = (status,message)=>{
+    const err = new Error()
+    err.status = status;
+    err.message = message;
+    return err;
 }
