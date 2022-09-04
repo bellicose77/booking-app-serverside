fbd4a418a96c9ec970df938fd1939a863f36106a
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-04 13:16:03 +0600

    user model

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
+import User from "../models/User.js"
+
 export const register = async(req,res,next)=>{
 try{
-    const newUser = new User();
+    const newUser = new User()
 
 }catch(err){
     next(err)
