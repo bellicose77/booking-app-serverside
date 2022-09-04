519e364d14b4f4f249755bb4713a65def5a29f30
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-04 12:05:38 +0600

    create conroller function

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,20 +1,12 @@
 import express from 'express';
+import { createHotel } from '../controllers/hotel.js';
 import Hotel from '../models/Hotel.js';
 import { createError } from '../utils/error.js';
 
 const router = express.Router()
 
 // Create api
-router.post("/",async(req,res)=>{
-    const newHotel = new Hotel(req.body)
-    try{
-        const savedHotel = await newHotel.save();
-        res.status(200).json(savedHotel);
-
-    }catch(err){
-          res.status(500).json(err);
-    }
-});
+router.post("/",createHotel);
 
 //update api 
 router.put('/:id', async(req,res)=>{
