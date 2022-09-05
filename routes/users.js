e36fdccf9095800f2956b24d8bc4d403e097f1d9
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-05 16:01:43 +0600

    verify admin

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,10 @@ router.get('/checkoutauthenticated',verifyToken,(req,res)=>{
 router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
    res.send("hello user, you are logged in and you can delete your account")
 });
+router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+      res.send("hello admin, you are logged in and you can delete all accounts")
+});
+    
 
 router.put('/:id',updateUser);
 //delete api
