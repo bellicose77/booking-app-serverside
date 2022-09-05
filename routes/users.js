563c7951c2f48c2a781f71b17ce68eb8dca2ee33
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-05 16:08:30 +0600

    verify admin

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,15 @@ import { verifyAdmin, verifyToken, verifyUser } from '../utils/verifyToken.js';
 const router = express.Router()
 
 //update api
-router.get('/checkoutauthenticated',verifyToken,(req,res)=>{
-    res.send("hello user, you'r authenticated")
-});
-router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
-   res.send("hello user, you are logged in and you can delete your account")
-});
-router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
-      res.send("hello admin, you are logged in and you can delete all accounts")
-});
+// router.get('/checkoutauthenticated',verifyToken,(req,res)=>{
+//     res.send("hello user, you'r authenticated")
+// });
+// router.get("/checkuser/:id", verifyUser, (req,res,next)=>{
+//    res.send("hello user, you are logged in and you can delete your account")
+// });
+// router.get("/checkadmin/:id", verifyAdmin, (req,res,next)=>{
+//       res.send("hello admin, you are logged in and you can delete all accounts")
+// });
     
 
 router.put('/:id',updateUser);
