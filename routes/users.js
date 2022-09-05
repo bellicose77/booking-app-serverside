ad8877e99230fe43280377cb3d905cbff3860168
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-05 13:27:42 +0600

    user route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { updateUser } from '../controllers/user';
+import { deleteUser, updateUser } from '../controllers/user';
 
 const router = express.Router()
 
 //update api
-router.put('/:id',updateUser)
+router.put('/:id',updateUser);
+//delete api
+router.delete('/:id',deleteUser)
 export default router
