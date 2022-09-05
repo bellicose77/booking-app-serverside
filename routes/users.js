7e8001690e83f9803a36cec8463418c9e9d00dbd
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-05 14:09:30 +0600

    verify token

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { deleteUser, getUser, getUsers, updateUser } from '../controllers/user';
+import { verifyToken } from '../utils/verifyToken.js';
 
 const router = express.Router()
 
 //update api
+router.get('/checkoutauthenticated',verifyToken,(req,res)=>{
+    res.send("hello user, you'r authenticated")
+})
 router.put('/:id',updateUser);
 //delete api
 router.delete('/:id',deleteUser);
