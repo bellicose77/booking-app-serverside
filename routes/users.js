e316b1a11f2204a2c2860980968069c758321b56
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-05 13:27:00 +0600

    user route

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { updateUser } from '../controllers/user';
 
 const router = express.Router()
 
-
+//update api
+router.put('/:id',updateUser)
 export default router
