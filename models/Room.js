433dc6c4b66fc0a814426debea7ced3088ab026c
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-06 09:01:02 +0600

    room model

--- models/Room.js
+++ models/Room.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const roomSchema = new  mongoose.Schema({
-    name:{
+    title:{
         type:String,
         required: true
     },
