b1e14671f8039fe2d24667e14ecea9d72ac398f0
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-06 09:10:23 +0600

    room controller

--- controllers/room.js
+++ controllers/room.js
@@ -3,4 +3,9 @@ import Room from "../models/Room.js";
 export const createRoom = async(req,res,next)=>{
     const hotelid = req.params.hotelid;
     const newRoom = new Room(req.body);
+    try{
+
+    }catch(err){
+        next();
+    }
 }
