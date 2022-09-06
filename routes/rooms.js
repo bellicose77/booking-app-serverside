18404665f4d15446bcd8ec5190241dd830056213
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-06 09:30:13 +0600

    room route

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { createRoom } from '../controllers/room';
+import { createRoom, deleteRoom, updateRoom } from '../controllers/room';
 import { verifyAdmin } from '../utils/verifyToken';
 
 const router = express.Router()
 
-router.post('/:id',verifyAdmin,createRoom)
+router.post('/:hotelid',verifyAdmin,createRoom);
+
+//update 
+router.put('/:id',verifyAdmin,updateRoom);
+//delete
+router.delete('/:id/:hotelid',verifyAdmin,deleteRoom)
 export default router
