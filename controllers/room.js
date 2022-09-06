c1a4a578968b8fcb27b5c8ee53e1e5adb68b6379
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-06 09:26:22 +0600

    room controller done

--- controllers/room.js
+++ controllers/room.js
@@ -25,8 +25,41 @@ export const updateRoom = async (req,res,next)=>{
             {$set:req.body},
             {new:true}
             )
-
-    }catch(err){
+        res.status(200).json(updatedRoom);
+    }
+    catch(err){
         next(err);
     }
-}
+};
+export const deleteRoom = async (req, res, next) => {
+    const hotelId = req.params.hotelid;
+    try {
+      await Room.findByIdAndDelete(req.params.id);
+      try {
+        await Hotel.findByIdAndUpdate(hotelId, {
+          $pull: { rooms: req.params.id },
+        });
+      } catch (err) {
+        next(err);
+      }
+      res.status(200).json("Room has been deleted.");
+    } catch (err) {
+      next(err);
+    }
+  };
+  export const getRoom = async (req, res, next) => {
+    try {
+      const room = await Room.findById(req.params.id);
+      res.status(200).json(room);
+    } catch (err) {
+      next(err);
+    }
+  };
+  export const getRooms = async (req, res, next) => {
+    try {
+      const rooms = await Room.find();
+      res.status(200).json(rooms);
+    } catch (err) {
+      next(err);
+    }
+  };
