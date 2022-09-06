b19119f68fa770afb2dfd04ecb7c7928848f5f39
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-06 09:11:33 +0600

    room controller

--- controllers/room.js
+++ controllers/room.js
@@ -4,6 +4,7 @@ export const createRoom = async(req,res,next)=>{
     const hotelid = req.params.hotelid;
     const newRoom = new Room(req.body);
     try{
+        const savedRoom = await newRoom.save()
 
     }catch(err){
         next();
