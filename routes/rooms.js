49923beb3004471db0b0bbc6273304a0e567c783
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-06 09:27:49 +0600

    room route

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import { createRoom } from '../controllers/room';
+import { verifyAdmin } from '../utils/verifyToken';
 
 const router = express.Router()
 
-
+router.post('/:id',verifyAdmin,createRoom)
 export default router
