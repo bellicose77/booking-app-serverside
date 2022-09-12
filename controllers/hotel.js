6fbda6f3597794eb478d81fb49be2c79e0678ac5
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 12:09:30 +0600

    count cities

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -52,10 +52,9 @@ export const getHotels = async (req,res,next)=>{
      }
 };
 export const countByCity = async (req,res,next)=>{
-    const cities = req.query.cities.split()
+    const cities = req.query.cities.split(",")
     try{
-        const hotels = await Hotel.find()
-         res.status(200).json(hotels);
+       
  
      }catch(err){
            next(err);
