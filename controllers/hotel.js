48cbf967e12ff36e5ac9470fedd0ca015bacda06
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 15:10:08 +0600

    getby type api

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -66,13 +66,9 @@ export const countByCity = async (req,res,next)=>{
      }
 };
 export const countByType = async (req,res,next)=>{
-    const cities = req.query.cities.split(",")
+   
     try{
-       const list = await Promise.all(
-        cities.map((city)=>{
-            return Hotel.countDocuments({city:city})
-        })
-        );
+       
         res.status(200).json(list);
  
      }catch(err){
