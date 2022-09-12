9ab99ac4b273f3cdb002d635040f67f451dc5dcd
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-12 16:41:09 +0600

    router dom in nav

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -45,7 +45,9 @@ export const getHotel = async (req,res,next)=>{
 export const getHotels = async (req,res,next)=>{
     const {min,max,...others} = req.query
     try{
-        const hotels = await Hotel.find().limit(req.query.limit)
+        const hotels = await Hotel.find({
+            ...others,chiptesPrice :{ $gt : min | 1 , $lt : max || 999}
+        }).limit(req.query.limit)
          res.status(200).json(hotels);
  
      }catch(err){
