0310f4351a16042fdb28d7b49fa711b869d91812
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-16 19:28:19 +0600

    get room api done

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -91,7 +91,7 @@ export const countByType = async (req,res,next)=>{
 
 export const getHotelRooms = async(req,res,next)=>{
     try{
-        const hotel = Hotel.findById(req.params.id);
+        const hotel = await Hotel.findById(req.params.id);
         const list = await Promise.all(hotel.rooms.map(room=>{
             return Room.findById(room)
         }))
