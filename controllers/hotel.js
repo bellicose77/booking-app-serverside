adcabd8477b9face519d0f4a238b9500de0c7812
Dulal halder <64400354+bellicose77@users.noreply.github.com>
2022-09-16 19:19:02 +0600

    rooms api

--- controllers/hotel.js
+++ controllers/hotel.js
@@ -1,5 +1,5 @@
 import Hotel from "../models/Hotel.js";
-
+import Room from "../models/Room.js";
 export const createHotel = async (req,res,next)=>{
     const newHotel = new Hotel(req.body)
     try{
@@ -91,7 +91,11 @@ export const countByType = async (req,res,next)=>{
 
 export const getHotelRooms = async(req,res,next)=>{
     try{
-
+        const hotel = Hotel.findById(req.params.id);
+        const list = await Promise.all(hotel.rooms.map(room=>{
+            return Room.findById(room)
+        }))
+        res.status(200).json(list)
     }catch(err){
         next(err)
     }
